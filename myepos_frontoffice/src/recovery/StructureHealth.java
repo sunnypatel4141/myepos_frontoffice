df05b141a0129c849790196aa56dfc2b360dd40b
sunnypatel4141 <User@TILL-03>
2014-07-26 22:24:49 +0100

    Created Structure Health

---
+++ myepos_frontoffice/src/recovery/StructureHealth.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 Sunny Patel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package recovery;
+
+import frontoffice.base.DBConnection;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ *
+ * @author Sunny Patel
+ */
+public class StructureHealth extends DBConnection {
+    String file = "schema.csv";
+    String line = "";
+    String splitBy = ",";
+    Object[][] modelSchema = null;
+    Integer counter = 0;
+    
+    StructureHealth() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while( ( line = br.readLine() ) != null ) {
+                modelSchema[counter] = line.split(splitBy);
+                counter++;
+            }
+        } catch(Exception a) {
+            
+        }
+    }
+    
+}
